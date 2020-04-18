658b61a19c5852e0d1d580d2bb28582a1e62a0e8
truls.michalsen@gmail.com <truls.michalsen@pm.me>
2020-04-18 08:01:59 +0200

    Add extra fields for property model and migrate the db

--- properties/models.py
+++ properties/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 class Property(models.Model):
-    adresse = models.CharField(max_length=200)
     finnkode = models.IntegerField()
     link = models.CharField(max_length=200)
+    adresse = models.CharField(max_length=200)
+    omrade1 = models.CharField(max_length=100)
+    omrade2 = models.CharField(max_length=100)
     pris = models.IntegerField()
+    fellesgjeld = models.IntegerField()
+    fellesformue = models.IntegerField()
+    omkostninger = models.IntegerField()
     totalpris = models.IntegerField()
+    felleskost = models.IntegerField()
     areal = models.IntegerField()
+    etasje = models.IntegerField()
+    byggear = models.IntegerField()
     megler = models.CharField(max_length=200)
     sold = models.BooleanField(default=False)
     actual_price = models.IntegerField(default=0)
